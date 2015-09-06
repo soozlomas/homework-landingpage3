93c1a04ad148dc6c5d77c2b47cd87d8fdd7d6d53
soozlomas <sooz@Soozs-MacBook-Pro.local>
2015-09-06 16:28:42 +1000

    initial jquery

---
+++ js/index.js
@@ -0,0 +1,31 @@
+//Prevent a form submission with the event.preventDefault() function
+//Use the $.ready() handler to delay your code from executing
+//until all DOM assets have been loaded
+
+
+$(document).ready(function(){
+
+$('.readmore').click(function() {
+  console.log('clicked 2');
+
+    $('.readmore').hide();
+    $('#show-this-on-click').slideDown(750);
+    $('.readless').show();
+});
+
+$('.readless').click(function() {
+  console.log('clicked 2');
+
+    $('#show-this-on-click').slideUp(750);
+    $('.readless').hide();
+    $('.readmore').fadeIn(1000);
+});
+
+$('.learnmore').click(function() {
+  console.log('clicked 2');
+
+    $('#learnmoretext').slideDown(750);
+    $('.learnmore').hide();
+});
+
+});
